5041045284811c953bb81c8bce5320b1548fb725
Chris-glary <148057690+Chris-glary@users.noreply.github.com>
2024-08-25 16:55:55 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const taskInput = document.getElementById('new-task');
+    const addTaskBtn = document.getElementById('add-task-btn');
+    const taskList = document.getElementById('task-list');
+
+    addTaskBtn.addEventListener('click', function () {
+        const taskText = taskInput.value.trim();
+        if (taskText !== '') {
+            addTask(taskText);
+            taskInput.value = '';
+        }
+    });
+   function addTask(taskText) {
+        const li = document.createElement('li');
+        li.textContent = taskText;
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Delete';
+        deleteBtn.classList.add('delete-btn');
+        li.appendChild(deleteBtn);
+        taskList.appendChild(li);
+    }
+});
